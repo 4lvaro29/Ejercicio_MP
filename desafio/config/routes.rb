77b006f3384850f783912b380c866f54c2d2802c
Alvaro Urbina <4lvaro.up@gmail.com>
2017-08-19 12:25:46 -0300

    second

--- desafio/config/routes.rb
+++ desafio/config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   get 'landings/contact'
 
-  root "landings#home"
+  root 'landings#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
